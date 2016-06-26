57af2baa966aa4957891fbce7274efb6510a0e6d
codetime66 <carlosandrefernandes@gmail.com>
2016-06-26 00:21:13 -0300

    some improvements on sign test code

--- bc-bind/src/bc/cipher/KeyPairTest.java
+++ bc-bind/src/bc/cipher/KeyPairTest.java
@@ -3,6 +3,7 @@
  */
 package bc.cipher;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -14,6 +15,7 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
@@ -74,15 +76,15 @@ public class KeyPairTest {
     }
 
     public static String readKeyFile(String fileName) throws Exception {
-            Reader in = new BufferedReader(
+        Reader in = new BufferedReader(
                 new InputStreamReader(new FileInputStream(fileName), "UTF8"));
-            StringBuffer buf = new StringBuffer();
-            int ch;
-            while ((ch = in.read()) > -1) {
-                buf.append((char)ch);
-            }
-            in.close();
-            return buf.toString();
+        StringBuffer buf = new StringBuffer();
+        int ch;
+        while ((ch = in.read()) > -1) {
+            buf.append((char) ch);
+        }
+        in.close();
+        return buf.toString();
     }
 
     public static void main(String[] args) throws Exception {
@@ -96,14 +98,55 @@ public class KeyPairTest {
         String pubK = readKeyFile("TSTPUBKEY");
         PublicKey pubSavedFile = loadPublicKey(pubK.toString());
         System.out.println(pair.getPublic() + "\n" + pubSavedFile);
-        
+
         String privKey = savePrivateKey(pair.getPrivate());
 
         writeKeyFile(privKey, "TSTPRIVKEY");
-        
+
         String privK = readKeyFile("TSTPRIVKEY");
         PrivateKey privSavedFile = loadPrivateKey(privK.toString());
         System.out.println(pair.getPrivate() + "\n" + privSavedFile);
-        
+
+        //
+        Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+        dsa.initSign(privSavedFile);
+        //
+        FileInputStream fis = new FileInputStream(args[0]);
+        BufferedInputStream bufin = new BufferedInputStream(fis);
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = bufin.read(buffer)) >= 0) {
+            dsa.update(buffer, 0, len);
+        };
+        bufin.close();
+        //
+        byte[] realSig = dsa.sign();
+        //
+        /* save the signature in a file */
+        FileOutputStream sigfos = new FileOutputStream("sig");
+        sigfos.write(realSig);
+        sigfos.close();
+
+        //
+        FileInputStream sigfis = new FileInputStream("sig");
+        byte[] sigToVerify = new byte[sigfis.available()];
+        sigfis.read(sigToVerify);
+        sigfis.close();
+        //
+        Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
+        sig.initVerify(pubSavedFile);
+        //
+        FileInputStream datafis = new FileInputStream(args[0]);
+        BufferedInputStream bufin2 = new BufferedInputStream(datafis);
+        byte[] buffer2 = new byte[1024];
+        int len2;
+        while (bufin2.available() != 0) {
+            len = bufin2.read(buffer2);
+            sig.update(buffer2, 0, len);
+        };
+        bufin2.close();
+        //
+        boolean verifies = sig.verify(sigToVerify);
+        System.out.println("signature verifies: " + verifies);
     }
 }
