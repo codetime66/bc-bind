07aee9719062885f80f4ab09093d80f91ee4ed27
codetime66 <carlosandrefernandes@gmail.com>
2016-06-26 14:46:08 -0300

    encrypt decrypt code

--- bc-bind/src/bc/cipher/RSADecryption.java
+++ bc-bind/src/bc/cipher/RSADecryption.java
@@ -1,52 +1,76 @@
 package bc.cipher;
 
-import bc.cipher.api.Command;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
 
 /**
  *
  * @author codetime
  */
-public class RSADecryption implements Command {
+public class RSADecryption implements bc.cipher.api.Cipher {
     
-    public void execute(byte[] message)
-            throws
-            NoSuchAlgorithmException,
-            NoSuchPaddingException,
-            InvalidKeyException,
-            BadPaddingException,
-            IllegalBlockSizeException {
-
-        // generating a 1024 bit RSA pair keys
-        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-        generator.initialize(1024);
-        KeyPair keys = generator.generateKeyPair();
+    @Override
+    public String perform(byte[] message, String keyFileName) throws Exception {
+        String privK = readKeyFile(keyFileName);
+        PrivateKey privSavedFile = loadPrivateKey(privK.toString());
+        System.out.println(privSavedFile);
 
         // implements RSA
-        Cipher cipher = Cipher.getInstance("RSA");
-
-        // encrypting the message using the public key
-        // initialize the cryptography algorithm
-        cipher.init(Cipher.ENCRYPT_MODE, keys.getPublic());
-
-        // encrypting the message
-        byte[] encryptedMensage = cipher.doFinal(message);
-
-        System.out.println("the encrypted mensage:");
-        System.out.println(new String(encryptedMensage));
+        javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("RSA","SUN");
 
         // decrypting the message using the private key
-        cipher.init(Cipher.DECRYPT_MODE, keys.getPrivate());
-        byte[] originalMessage = cipher.doFinal(encryptedMensage);
+        // initialize the cryptography algorithm        
+        cipher.init(javax.crypto.Cipher.DECRYPT_MODE, privSavedFile);
+        
+        // decrypting the message        
+        byte[] originalMessage = cipher.doFinal(message);
+        
         System.out.println("the original message was:");
         System.out.println(new String(originalMessage));
+        
+        return new String(originalMessage);
+    }
 
+    protected String readKeyFile(String fileName) throws Exception {
+        Reader in = new BufferedReader(
+                new InputStreamReader(new FileInputStream(fileName), "UTF8"));
+        StringBuffer buf = new StringBuffer();
+        int ch;
+        while ((ch = in.read()) > -1) {
+            buf.append((char) ch);
+        }
+        in.close();
+        return buf.toString();
+    }
+
+    protected PrivateKey loadPrivateKey(String key64) throws GeneralSecurityException {
+        byte[] clear = base64Decode(key64);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(clear);
+        KeyFactory fact = KeyFactory.getInstance("DSA", "SUN");
+        PrivateKey priv = fact.generatePrivate(keySpec);
+        Arrays.fill(clear, (byte) 0);
+        return priv;
+    }
+
+    protected byte[] base64Decode(String key64) {
+        return Base64.getDecoder().decode(key64);
+    }
+    
+    @Override
+    public String perform(byte[] message) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String perform(byte[] message, byte[] hashFile, String keyFileName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
--- bc-bind/src/bc/cipher/RSAEncryption.java
+++ bc-bind/src/bc/cipher/RSAEncryption.java
@@ -1,40 +1,33 @@
 package bc.cipher;
 
-import bc.cipher.api.Command;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 /**
  *
  * @author codetime
  */
-public class RSAEncryption implements Command {
+public class RSAEncryption implements bc.cipher.api.Cipher {
     
-    public void execute(byte[] message)
-            throws
-            NoSuchAlgorithmException,
-            NoSuchPaddingException,
-            InvalidKeyException,
-            BadPaddingException,
-            IllegalBlockSizeException {
-
-        // generating a 1024 bit RSA pair keys
-        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-        generator.initialize(1024);
-        KeyPair keys = generator.generateKeyPair();
+    @Override
+    public String perform(byte[] message, String keyFileName) throws Exception {
+        String pubK = readKeyFile(keyFileName);
+        PublicKey pubSavedFile = loadPublicKey(pubK.toString());
+        System.out.println(pubSavedFile);
 
         // implements RSA
-        Cipher cipher = Cipher.getInstance("RSA");
+        javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("RSA","SUN");
 
         // encrypting the message using the public key
         // initialize the cryptography algorithm
-        cipher.init(Cipher.ENCRYPT_MODE, keys.getPublic());
+        cipher.init(javax.crypto.Cipher.ENCRYPT_MODE, pubSavedFile);
 
         // encrypting the message
         byte[] encryptedMensage = cipher.doFinal(message);
@@ -42,11 +35,41 @@ public class RSAEncryption implements Command {
         System.out.println("the encrypted mensage:");
         System.out.println(new String(encryptedMensage));
 
-        // decrypting the message using the private key
-        cipher.init(Cipher.DECRYPT_MODE, keys.getPrivate());
-        byte[] originalMessage = cipher.doFinal(encryptedMensage);
-        System.out.println("the original message was:");
-        System.out.println(new String(originalMessage));
-
+        return new String(encryptedMensage);
     }
+
+    protected PublicKey loadPublicKey(String stored) throws GeneralSecurityException {
+        byte[] data = base64Decode(stored);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+        KeyFactory fact = KeyFactory.getInstance("DSA","SUN");
+        return fact.generatePublic(spec);
+    }
+    
+    protected byte[] base64Decode(String key64) {
+        return Base64.getDecoder().decode(key64);
+    }
+    
+    protected String readKeyFile(String fileName) throws Exception {
+        Reader in = new BufferedReader(
+                new InputStreamReader(new FileInputStream(fileName), "UTF8"));
+        StringBuffer buf = new StringBuffer();
+        int ch;
+        while ((ch = in.read()) > -1) {
+            buf.append((char) ch);
+        }
+        in.close();
+        return buf.toString();
+    }
+
+    @Override
+    public String perform(byte[] message) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public String perform(byte[] message, byte[] hashFile, String keyFileName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+
 }
